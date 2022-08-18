ac0b4aa40f0494c37c6f4f2b02c66170b5a1793a
neach128 <neach128@gmail.com>
2022-08-18 12:51:47 +0100

    first commit

---
+++ home.js
@@ -0,0 +1,47 @@
+
+
+        // var b = 'neche';
+        // console.log(b);
+
+        // var someNumber = 45;
+        // console.log(someNumber)
+
+        // document.getElementById('someText').innerHTML = 'hello';
+
+        // var age = prompt('what is your age');
+        // document.getElementById('someText').innerHTML = age;
+
+        // console.log(age)
+
+
+        // var num1 = 10;
+
+        // num1 = num1 + 1;
+
+        // console.log(num1);
+
+        // var num1= 10;
+        // num1--;
+        // console.log(num1);
+
+
+        // function greeting() {
+        //         var name = prompt('what s your name?');
+        //         var result = 'Hello' + ' ' + name;
+        //         console.log(result);
+        // }
+
+        // console.log('Hello');
+
+        // function sumNumber(num1, num2){
+        //         var result = num1 + num2;
+        //         console.log(result);
+        // }
+
+        // var full = prompt('What is your name'){
+        //         var result = ('hello + full');
+        //         console.log();
+        // }
+        
+        
+
